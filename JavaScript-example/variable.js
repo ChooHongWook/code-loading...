17b8ab8e4ccd339fe1bb8528ceba9de9c7f79ed3
olt13mm <olt13mm@gmail.com>
2021-11-10 22:13:50 +0900

    [first] 문제를 처음만듬

---
+++ JavaScript-example/variable.js
@@ -0,0 +1,20 @@
+// 연습파일 예시들
+
+// 1. 키워드 let을 사용하여 변수 result를 선언하고 
+//    문자열 'study hard'를 할당하세요.
+let result;
+result = 'study hard';
+
+// 2. 변수 num1에 숫자 3를 할당하고, 변수 num2에 숫자 5을 할당한 후, 
+//    변수 result에 숫자가 할당된 변수 num1과 num2의 곱을 할당합니다.
+
+let num1 = 3 , num2 = 5, result =  num1 * num2;
+
+// 3. 정사각형의 한면의 길이를 알때 정사각형의 넓이를 구하는 함수를 만들어라
+
+function getSquareArea(length) {
+  let squareArea = length ** 2
+
+  return distance;
+}
+
