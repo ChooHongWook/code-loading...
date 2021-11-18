184dea0979e0f15ea166d726106c653755e89a1a
olt13mm <olt13mm@gmail.com>
2021-11-18 12:44:09 +0900

    [쓰는 연습]깃 에디터 잘작동됨?

--- practicing.js
+++ practicing.js
@@ -1,2 +1,32 @@
-console.log('뭘봐')
+function listPrimes(num) {
+  // TODO: 여기에 코드를 작성합니다.
+  // function isPrime(num) {
+  //   for (let i = 2; i < num; i ++) {
+  //     if ( num % i === 0) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+  // /*--------------------*/
+  // let result = '2'
+  // for(let i = 3; i <= num; i++) {
+  //   if (isPrime(i) === true) {
+  //     result = result + '-' + i;
+  //   }
+  // }
+  // return result;
 
+  let result = '2'
+  for(let i = 3; i <= num; i++) {
+    for (let j = 2; j < i; j ++) {
+      if ( i % j === 0) {
+        break;
+      }
+    }
+    result = result + '-' + i;
+  }
+  return result;
+
+
+}
